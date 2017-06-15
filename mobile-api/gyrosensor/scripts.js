d6c5d5befb43c01941f02de1e560af6b143391c0
後藤和良 <goto@gotoukazuyoshi-no-iMac.local>
2017-06-15 13:21:42 +0900

    update

--- mobile-api/gyrosensor/scripts.js
+++ mobile-api/gyrosensor/scripts.js
@@ -15,19 +15,19 @@
     //ジャイロセンサー情報取得
     // beta = X軸
     var beta = event.beta;
-    // gamma = Y軸
-    var gamma = event.gamma;
-    // alpha = Z軸
+    // alpha = Y軸
     var alpha = event.alpha;
+    // gamma = Z軸
+    var gamma = event.gamma;
     var html = "";
     html += "X回転 : " + beta + "<br>";
-    html += "Y回転 : " + gamma + "<br>";
-    html += 'Z回転 : ' + alpha;
+    html += "Y回転 : " + alpha + "<br>";
+    html += 'Z回転 : ' + gamma;
     $("#debug").html(html);
 
     $orientationObj.css({
-      "-webkit-transform": "rotateX(" + (180 + beta) + "deg) rotateY(" + (180 + gamma) + "deg) rotateZ(" + alpha + "deg)",
-      "transform": "rotateX(" + (180 + beta) + "deg) rotateY(" + (180 + gamma) + "deg) rotateZ(" + alpha + "deg)"
+      //betaを-90度調整することで、スマホを立てた状態で正面にしている
+      "transform": "rotateX(" + (-90 + beta) + "deg) rotateY(" + alpha + "deg) rotateZ(" + gamma + "deg)"
     })
   }
 })();

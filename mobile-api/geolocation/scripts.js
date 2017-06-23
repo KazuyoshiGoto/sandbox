0ae44cf702b0f8357e7052f60563399b31db3a82
Kazuyoshi Goto <aimluck.goto@gmail.com>
2017-06-23 21:03:59 +0900

    update

--- mobile-api/geolocation/scripts.js
+++ mobile-api/geolocation/scripts.js
@@ -12,6 +12,8 @@ function initMap() {
   navigator.geolocation.getCurrentPosition(function(position) {
     // 緯度経度の取得
     latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    console.log(position);
+    console.log(latLng);
 
     // 地図の作成
     map = new google.maps.Map(document.getElementById('map'), {
